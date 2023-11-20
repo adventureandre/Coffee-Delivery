181c3479fc358cb886c2bd8520ab44634f2a24a5
Adventureandre <adventureandre@hotmail.com>
2023-11-19 23:16:08 -0300

    Header ok!

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
-import {Container} from "./styles.ts";
+import {Aside, Container, Location} from "./styles.ts";
+import {Link} from "react-router-dom";
+import {MapPin, ShoppingCart} from "phosphor-react";
 
 export function Header() {
-    return(
-       <Container>
-
-       </Container>
+    return (
+        <Container>
+            <Link to="/">
+                <img src="/logo.svg" alt="Coffee Delivery"/>
+            </Link>
+            <Aside>
+                <Location><MapPin size={22} weight="fill"/> <span>Goiania, GO</span></Location>
+                <Link to="/cart" aria-disabled={true} ><ShoppingCart size={22} weight="fill"/><span>3</span></Link>
+            </Aside>
+        </Container>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,10 +2,57 @@ import styled from "styled-components";
 
 export const Container  = styled.header`
   max-width: 1100px;
-  padding: 32px 20px;
+  padding: 30px 20px;
   margin: 0 auto;
 
   display: flex;
   justify-content: space-between;
   align-items: center;
+`
+
+export const Aside = styled.aside`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 12px;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    background-color: ${({ theme }) => theme.colors['yellow-light']};
+    color: ${({ theme }) => theme.colors['yellow-dark']};
+
+    padding: 8px;
+    border-radius: 6px;
+
+    position: relative;
+
+    &[aria-disabled='true'] {
+      pointer-events: none;
+    }
+    span{
+      color: ${(props) => props.theme.colors['white']};
+      text-align: center;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background: ${(props) => props.theme.colors['yellow-dark']};
+      
+      position: absolute;
+      right: -8.345px;
+      top: -8px;
+    }
+  }
+`
+export const Location = styled.div`
+  display: flex;
+  padding: 8px;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  
+  color: ${(props) => props.theme.colors["purple-dark"]};
+  background: ${(props) => props.theme.colors["purple-light"]};
+  border-radius: 6px;
 `
